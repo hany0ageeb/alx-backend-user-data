3b5ecbef8c13ce54143a78c860eb9fe6daef3d50
hany0ageeb <hany0ageeb@gmail.com>
2024-11-07 20:47:55 +0200

    Task 3. Connect to secure database

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,11 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(
         password=password.encode('utf-8'),
         salt=bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(
+        password=password.encode('utf-8'),
+        hashed_password=hashed_password)
