396a478a7b57bb2bdc64f0af4337710542999a12
hany0ageeb <hany0ageeb@gmail.com>
2024-11-07 18:50:26 +0200

    Task 1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,8 +13,9 @@ Arguments:
     - filter_datum should be less than 5 lines long
     and use re.sub to perform the substitution with a single regex.
 """
-from typing import (List)
+from typing import List
 import re
+import logging
 
 
 def filter_datum(
@@ -27,13 +28,27 @@ def filter_datum(
         fields: list of strings representing all fields to obfuscate
     """
     for field in fields:
-        message = re.sub(r'{}=(.+?){}'.format(field, separator),
+        message = re.sub(r'{}=.+?{}'.format(field, separator),
                          f'{field}={redaction}{separator}', message)
     return message
 
 
-if __name__ == '__main__':
-    message = 'field=123;field=123;'
-    print(filter_datum(["field", "filed"],
-          'xxxx', message, ";"))
-    print(message)
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        formatter = logging.Formatter(self.FORMAT)
+        return formatter.format(record)
