c43a55f8ad2a17c91e7eac95ed40b34d50829074
hany0ageeb <hany0ageeb@gmail.com>
2024-11-11 18:47:47 +0200

    3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# auth.py
+"""a class to manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False
+        """
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        """that returns None
+        Args:
+            request: the flas request object
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None
+        Args:
+            request: the flas request object
+        """
+        return None
