1bd857ce74af81ab0d31dc8011866864e699bd28
hany0ageeb <hany0ageeb@gmail.com>
2024-11-11 19:56:47 +0200

    Task 6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# basic_auth.py
+"""class BasicAuth that inherits from Auth
+"""
+from auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class BasicAuth
+    """
+    pass
