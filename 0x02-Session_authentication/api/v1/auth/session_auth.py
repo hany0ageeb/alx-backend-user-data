44eb97f7642ecdf4c1a2231ba62b68d9181b7b7f
hany0ageeb <hany0ageeb@gmail.com>
2024-11-18 19:44:25 +0200

    Task 2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 # session_auth.py
 """class SessionAuth that inherits from Auth
+Create a class attribute user_id_by_session_id
+initialized by an empty dictionary
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """class SessionAuth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id: str = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
