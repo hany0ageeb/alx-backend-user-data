6519730ad6ecda3e29239a21b02909d0cc779750
hany0ageeb <hany0ageeb@gmail.com>
2024-11-18 21:17:43 +0000

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import MultipleResultsFound
 
 from user import Base, User
 
@@ -38,3 +39,11 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """fnd_by_user
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except MultipleResultsFound:
+            return self._session.query(User).filter_by(**kwargs).first()
