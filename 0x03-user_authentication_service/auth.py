6e121a82a737faa6fc51b2f0146a5e0c95c6ff17
hany0ageeb <hany0ageeb@gmail.com>
2024-11-19 11:12:06 +0000

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,6 +20,8 @@ class Auth:
     """
 
     def __init__(self):
+        """__init__
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
@@ -31,3 +33,15 @@ class Auth:
                 raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Expect email and password required arguments
+        Returns a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
