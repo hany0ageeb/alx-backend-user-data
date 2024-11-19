5eb3ad8346eb2637aa5ecafae19e6fdf7aa9e3bd
hany0ageeb <hany0ageeb@gmail.com>
2024-11-19 06:52:09 +0000

    Task 1. add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,13 +35,14 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """save the user to the database
         """
+        session = self._session
         try:
             user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
+            session.add(user)
+            session.commit()
             return user
         except Exception:
-            self._session.rollback()
+            session.rollback()
             return None
 
     def find_user_by(self, **kwargs):
